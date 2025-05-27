518e66f69114943846b56858c58fef59282a0680
Roope Kallio <5762764+ropelli@users.noreply.github.com>
2025-05-27 13:30:57 +0300

    copilot keymaps to jkl

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,23 @@
 return {
-  { "github/copilot.vim" },
+  {
+    'github/copilot.vim',
+
+    config = function()
+      vim.keymap.set('i', '<M-j>', 'copilot#Next()', {
+        desc = 'Copilot Previous Suggestion',
+        expr = true,
+        silent = true,
+      })
+      vim.keymap.set('i', '<M-k>', 'copilot#Previous()', {
+        desc = 'Copilot Previous Suggestion',
+        expr = true,
+        silent = true,
+      })
+      vim.keymap.set('i', '<M-l>', 'copilot#AcceptWord()', {
+        desc = 'Copilot Accept Word',
+        expr = true,
+        silent = true,
+      })
+    end,
+  },
 }
