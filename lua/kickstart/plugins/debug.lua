e7a347c1fb47ec9450c27fdb83732f85e6b8de72
Roope Kallio <5762764+ropelli@users.noreply.github.com>
2025-09-26 10:07:38 +0300

    add godot debugger attach configuration

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -58,6 +58,13 @@ return {
         project = '${workspaceFolder}',
         launch_scene = true,
       },
+      {
+        type = 'godot',
+        request = 'attach',
+        name = 'Attach to running Godot instance',
+        host = '127.0.0.1',
+        port = 6006,
+      },
     }
     dap.adapters.godot = {
       type = 'server',
