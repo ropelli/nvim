099f0a677550a1f79035a7f445e8364ab1955de3
Roope Kallio <5762764+ropelli@users.noreply.github.com>
2025-10-22 10:35:06 +0300

    fix copilotchat key mappings

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -10,12 +10,20 @@ return {
     build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
       -- See Configuration section for options
+      mappings = {
+        reset = {
+          insert = '',
+        },
+      },
     },
     -- See Commands section for default commands if you want to lazy load on them
     keys = {
-      { '<leader>cpe', '<cmd>CopilotChatExplain<cr>', desc = 'Copilot Explain' },
-      { '<leader>cpf', '<cmd>CopilotChatFix<cr>', desc = 'Copilot Fix' },
-      { '<leader>cpc', '<cmd>CopilotChat<cr>', desc = 'Copilot Chat' },
+      { '<leader>cpe', '<cmd>CopilotChatExplain<cr>', mode = { 'n', 'v' }, desc = 'Copilot Explain' },
+      { '<leader>cpf', '<cmd>CopilotChatFix<cr>', mode = { 'n', 'v' }, desc = 'Copilot Fix' },
+      { '<leader>cpc', '<cmd>CopilotChat<cr>', mode = { 'n', 'v' }, desc = 'Copilot Chat' },
+      { '<leader>cpr', '<cmd>CopilotChatReset<cr>', mode = { 'n', 'v' }, desc = 'Copilot Chat Reset' },
+      { '<leader>cpq', '<cmd>CopilotChatClose<cr>', mode = { 'n', 'v' }, desc = 'Copilot Chat Close' },
+      { '<leader>cpo', '<cmd>CopilotChatOptimize<cr>', mode = { 'n', 'v' }, desc = 'Copilot Chat Optimize' },
     },
   },
 }
