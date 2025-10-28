a5814524e272b7b4883c5c90cdb08b5c9d7a4c1b
Roope Kallio <5762764+ropelli@users.noreply.github.com>
2025-10-28 16:12:28 +0200

    in Oil, <leader>` opens current directory in a new nvim instance with a tmux horizontal split

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -8,5 +8,20 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   keys = {
     { '-', '<cmd>Oil<cr>', desc = 'Oil' },
+    {
+      '<leader>`',
+      function()
+        local oil = require 'oil'
+        local dir = oil.get_current_dir()
+        if dir == nil then
+          print 'Could not determine current directory'
+          return
+        end
+        local cmd_template = 'nvim -c "cd {dir}" -c ":e ."'
+        local cmd = cmd_template:gsub('{dir}', dir)
+        os.execute("tmux split-window -h '" .. cmd .. "'")
+      end,
+      desc = "Open a new nvim panel with the current buffer's directory using tmux",
+    },
   },
 }
