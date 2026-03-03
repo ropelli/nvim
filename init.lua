def87f02d6d1318e3af7e7c928bcb42b4aae917d
Roope Kallio <5762764+ropelli@users.noreply.github.com>
2026-03-03 14:40:12 +0200

    add lsps

--- init.lua
+++ init.lua
@@ -713,8 +713,17 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        asm_lsp = {},
+        bashls = {},
         clangd = {},
+        docker_compose_language_service = {},
+        dockerls = {},
         gopls = {},
+        html = {},
+        java_language_server = {},
+        lemminx = {},
+        perlnavigator = {},
+        templ = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
